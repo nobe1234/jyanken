9d00837056ed02da2883bc49b79d565608943b45
nobe1234 <sohei.nobe@rakus.co.jp>
2019-08-23 14:20:47 +0900

    じゃんけんアプリを作成

--- src/index.js
+++ src/index.js
@@ -1,12 +1,64 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
+import PropTypes from 'prop-types';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+class JyankenGamePage extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {human: null, computer: null}
+    }
+    pon(human_hand) {
+        const computer_hand = Math.floor(Math.random() * 3)
+        this.setState({human: human_hand, computer: computer_hand})
+    }
+    judge(){
+        if(this.state == null){
+            return null
+        } else {
+            return (this.state.computer - this.state.human + 3) % 3
+        }
+    }
+    render(){
+        return(
+            <div>
+                <h1>じゃんけんぽん！</h1>
+                <JyankenBox actionPon={(te) => this.pon(te)} />
+                <ScoreBox human={this.state.human} computer={this.state.computer} judgement={this.judge()} />
+            </div>
+        )
+    }
+}
+    const JyankenBox = (props) => {
+        return (
+            <div>
+                <button onClick={() => props.actionPon(0)}>グー</button>
+                <button onClick={() => props.actionPon(1)}>チョキ</button>
+                <button onClick={() => props.actionPon(2)}>パー</button>
+            </div>
+        )
+    }
+    JyankenBox.propTypes = {
+        actionPon: PropTypes.func
+    }
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+    const ScoreBox = (props) => {
+        const teString = ["グー","チョキ","パー"];
+        const judgementString = ["引き分け","勝ち","負け"];
+        return (
+            <table>
+                <tbody>
+                    <tr><th>あなた</th><td>{teString[props.human]}</td></tr>
+                    <tr><th>コンピューター</th><td>{teString[props.computer]}</td></tr>
+                    <tr><th>勝敗</th><td>{judgementString[props.judgement]}</td></tr>
+                </tbody>
+            </table>
+        )
+    }
+    ScoreBox.propTypes = {
+        human: PropTypes.number,
+        computer: PropTypes.number,
+        judgement: PropTypes.number
+    }
+
+ReactDOM.render(<JyankenGamePage />, document.getElementById('root'));
